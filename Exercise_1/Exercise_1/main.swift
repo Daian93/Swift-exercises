1ba605da773eb226d55f73d31c8fe784d019eaf1
Diana Rammal Sansón <dianarammal93@gmail.com>
2025-09-10 00:52:36 +0200

    Exercise 1

---
+++ Exercise_1/Exercise_1/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//
+//  Created by Diana Rammal Sansón on 10/9/25.
+//
+//  Introduction to Programming
+//  Algorithm Exercises
+//  Block 1: Getting Started
+//
+//  Exercise 1: Create an algorithm that asks the user for two numbers and displays their sum.
+
+print("Ingresa el primer número: ", terminator: "")
+let num1 = Int(readLine() ?? "") ?? 0
+
+print("Ingresa el segundo número: ", terminator: "")
+let num2 = Int(readLine() ?? "") ?? 0
+
+let result = num1 + num2
+print("\(num1) + \(num2) = \(result)")
